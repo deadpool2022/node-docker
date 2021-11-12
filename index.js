5d3fb79a5cd664778a6464b9b68c365354dd997c
vikas <vikas.ep@globallogic.com>
2021-11-12 07:09:46 -0800

    basic crud api is ready

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+
+const postRouter = require("./routes/postRoutes");
+
 const app = express();
 
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`;
@@ -15,16 +23,17 @@ const retryToConnect = () => {
     .catch((e) => {
       console.log(e);
       setTimeout(retryToConnect, 5000);
-    }
-    );
-}
+    });
+};
 
 retryToConnect();
 
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h2>Hey There!!! boogiie boogiie woogie woo woo woo cool!!!<h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.PORT || 3000;
 
